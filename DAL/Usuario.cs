77b2e9a1c1b7a28c890f639342060bc9fa7e283c
Yaggi <yaggim@gmail.com>
2025-04-11 18:12:44 -0300

    Primer commit

---
+++ DAL/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DAL
+{
+    public class Usuario
+    {
+        public DataTable IniciarSesion()
+        {
+
+            Conexion conexion = new Conexion();
+
+            DataTable dt = conexion.LeerPorComando("SELECT usuario, clave FROM [Universidad].[dbo].[Usuario]");
+
+            return dt;
+        }
+    }
+}
---
+++ LoginPrueba/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginPrueba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            string usuario = tboxUsuario.Text;
+            string clave = tboxClave.Text;
+
+            BLL.Usuario unLogin = new BLL.Usuario();
+
+            if(unLogin.Loguearse(usuario, clave))
+            {
+                MessageBox.Show("Inicio sesión correctamente");
+            }
+            else
+            {
+                MessageBox.Show("El usuario o la clave es incorrecta");
+            }
+        }
+    }
+}
